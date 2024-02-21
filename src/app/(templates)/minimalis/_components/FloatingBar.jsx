0b74f19ceb22fd4dd844c8b5de6f2669ee613e5f
Jfraska <davinfraska20@gmail.com>
2024-02-21 18:28:19 +0700

    fix: slide button

--- src/app/(templates)/minimalis/_components/FloatingBar.jsx
+++ src/app/(templates)/minimalis/_components/FloatingBar.jsx
@@ -1,22 +1,17 @@
 "use client";
 
+import Image from "next/image";
+
 export default function FloatingBar() {
   return (
     <div className="sticky flex justify-center items-center bottom-20 left-0 mx-5 z-40 w-10 h-10 border border-transparent rounded-full bg-gray-800 backdrop-filter backdrop-blur-md bg-opacity-60">
-      <svg
-        width="14"
-        height="21"
-        viewBox="0 0 22 29"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M2 6.3983V23.02M8 2V27M14 20.7457V8.23709M20 10.3057V18.4714"
-          stroke="white"
-          stroke-width="3"
-          stroke-linecap="round"
-        />
-      </svg>
+      <Image
+        src={"/vercel.svg"}
+        alt="music"
+        width={20}
+        height={20}
+        className="w-auto h-auto"
+      />
     </div>
   );
 }
