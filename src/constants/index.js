acb8c501d4150c3948cbb79a7b7f283d4f23cb96
Jfraska <davinfraska20@gmail.com>
2024-02-25 00:28:38 +0700

    fix: faq

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,29 +111,29 @@ export const price = [
 
 export const faq = [
   {
-    question: "How many programmers does it take to screw a lightbulb?",
+    question: "Bagaimana cara order",
+    answer: "Klik icon cart dan nanti akan diarahkan ke whatsapp kami",
+    open: false,
+  },
+  {
+    question: "Apakah bisa menghilangkan bagian yang tidak digunakan",
+    answer: "Bisa, bagian yang tidak digunakan bisa dihilangkan",
+    open: false,
+  },
+  {
+    question: "Berapa lama waktu pengerjaan",
+    answer: "Pengerjaan 2-3 hari, setelah data dan foto kami terima.",
+    open: false,
+  },
+  {
+    question: "Kata-kata dalam undangan bisa diganti",
+    answer: "Semua teks  dapat diubah sesuai kebutuhan",
+    open: false,
+  },
+  {
+    question: "Pengembalian dana",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-    open: false,
-  },
-  {
-    question: "Who is the most awesome person?",
-    answer: "You! The viewer!",
-    open: false,
-  },
-  {
-    question: "How many questions does it take to makes a succesful FAQ Page?",
-    answer: "This many!",
-    open: false,
-  },
-  {
-    question: "How many questions does it take to makes a succesful FAQ Page?",
-    answer: "This many!",
-    open: false,
-  },
-  {
-    question: "How many questions does it take to makes a succesful FAQ Page?",
-    answer: "This many!",
+      "Setiap pembelian tidak bisa di refund. Ganti tamplate masih dapat dilakukan dengan ketentuan undangan belum dikerjakan",
     open: false,
   },
 ];
