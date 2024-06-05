9ca5dfdab14d79459c8fdb7a4517b199e9ed737f
Jfraska <davinfraska20@gmail.com>
2024-06-05 09:26:19 +0700

    fix: update prisma

--- src/app/template/minimalis/page.js
+++ src/app/template/minimalis/page.js
@@ -5,28 +5,28 @@ import Story from "./_components/Story";
 import Event from "./_components/Event";
 import LockScreen from "./_components/LockScreen";
 import FloatingBar from "./_components/FloatingBar";
-import prisma from "@/libs/prisma";
+// import prisma from "@/libs/prisma";
 import Thanks from "./_components/Thanks";
 import Galery from "./_components/Galery";
 import LiveStream from "./_components/LiveStream";
 import Cover from "./_components/Cover";
 
-async function getInvitation() {
-  const invitation = await prisma.Invitation.findFirst({
-    where: { title: "default" },
-  });
-  return invitation;
-}
+// async function getInvitation() {
+//   const invitation = await prisma.Invitation.findFirst({
+//     where: { title: "default" },
+//   });
+//   return invitation;
+// }
 
 export default async function Page() {
   // const result = await getInvitation();
 
   return (
     <>
-      <Cover />
+      {/* <Cover /> */}
       <div className="relative w-full md:max-w-[430px] overflow-y-auto bg-color">
-        <LockScreen />
-        <Beranda />
+        {/* <LockScreen /> */}
+        {/* <Beranda /> */}
         <Quotes />
         <Couple />
         <Story />
