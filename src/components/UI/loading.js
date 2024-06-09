af6dcc242f89ec81762b55e38f67a4182208a7fd
Jfraska <davinfraska20@gmail.com>
2024-06-09 09:42:28 +0700

    feat: add seeder

--- src/components/UI/loading.js
+++ src/components/UI/loading.js
@@ -2,9 +2,7 @@ import { Backdrop, CircularProgress } from "@mui/material";
 
 export default function Loading() {
   return (
-    <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-    >
+    <Backdrop sx={{ color: "#fff" }}>
       <CircularProgress color="inherit" />
     </Backdrop>
   );
