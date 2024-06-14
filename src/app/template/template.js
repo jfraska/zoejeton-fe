965dbf2969562e59a0b9d861e45a7c376b64fc7a
Jfraska <davinfraska20@gmail.com>
2024-06-14 11:18:01 +0700

    fix: update template page

--- src/app/template/template.js
+++ src/app/template/template.js
@@ -11,7 +11,7 @@ import DrawerShare from "@/components/container/drawer-share";
 export default function Template({ children }) {
   const [isLoading, setIsLoading] = useState(true);
   const pathname = usePathname();
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
   const { setDataContent, dataColor, setDataColor } =
     useContext(CustomizeContext);
 
@@ -22,13 +22,13 @@ export default function Template({ children }) {
         let result = await response.json();
         setDataContent(result.data);
         setDataColor(result.data.color[0]);
-        if (searchParams) {
-          response = await fetch(
-            `/api/guest/${searchParams?.get("guest")}?slug=${pathname}`
-          );
-          result = await response.json();
-          setDataGuest(result.data);
-        }
+        // if (searchParams) {
+        //   response = await fetch(
+        //     `/api/guest/${searchParams?.get("guest")}?slug=${pathname}`
+        //   );
+        //   result = await response.json();
+        //   setDataGuest(result.data);
+        // }
       } catch (error) {
         console.log("Error fetching data:", error);
       } finally {
