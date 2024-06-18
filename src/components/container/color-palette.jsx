a313e18eba0a2aa75dfcde8d2584d0f40f867dbf
Jfraska <davinfraska20@gmail.com>
2024-06-18 15:24:02 +0700

    fix: update feature customize

--- src/components/container/color-palette.jsx
+++ src/components/container/color-palette.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import ColorPicker from "@/components/container/color-picker";
 import { rgbToHex } from "@mui/material";
-import { getPaletteFromURL } from "color-thief-node";
+import ColorThief from "color-thief-ts/node";
 
 export default function ColorPalette({ colors, setColor }) {
   const [palette, setPalette] = useState([]);
+  const colorThief = new ColorThief();
 
   const handleColorChange = (e, field) => {
     const updatedData = {
@@ -18,25 +19,27 @@ export default function ColorPalette({ colors, setColor }) {
     setColor(updatedData);
   };
 
-  // useEffect(() => {
-  //   const imgElements = document.querySelectorAll(".image");
-  //   let colors = [];
+  useEffect(() => {
+    const imgElements = document.querySelectorAll(".image");
+    let colors = [];
 
-  //   const fetchPalettes = async () => {
-  //     const promises = Array.from(imgElements).map(async (img) => {
-  //       const colorPalette = await getPaletteFromURL(img.getAttribute("src"));
-  //       return colorPalette.map((color) =>
-  //         rgbToHex(`rgb(${color[0]},${color[1]},${color[2]})`)
-  //       );
-  //     });
+    const fetchPalettes = async () => {
+      const promises = Array.from(imgElements).map(async (img) => {
+        const colorPalette = await colorThief.getPalette(
+          img.getAttribute("src")
+        );
+        return colorPalette.map((color) =>
+          rgbToHex(`rgb(${color[0]},${color[1]},${color[2]})`)
+        );
+      });
 
-  //     const results = await Promise.all(promises);
-  //     colors = results.flat();
-  //     setPalette(colors);
-  //   };
+      const results = await Promise.all(promises);
+      colors = results.flat();
+      setPalette(colors);
+    };
 
-  //   fetchPalettes();
-  // }, []);
+    fetchPalettes();
+  }, []);
 
   return (
     <div className="bg-white p-2 flex flex-wrap gap-2 rounded">
