bbfd65aa8409d24a65e29c7d23428197b847d86c
Jfraska <davinfraska20@gmail.com>
2024-06-25 09:47:44 +0700

    fix: update loading template

--- src/app/template/loading.js
+++ src/app/template/loading.js
@@ -5,23 +5,6 @@ import Image from "next/image";
 import { Progress } from "@/components/UI/progress";
 
 export default function Loading() {
-  const [progress, setProgress] = useState(8);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prev) => {
-        if (prev < 100) {
-          return prev + 1;
-        } else {
-          clearInterval(timer);
-          return prev;
-        }
-      });
-    }, 2000 / (100 - 8));
-
-    return () => clearInterval(timer);
-  }, []);
-
   return (
     <div className="relative w-full h-screen bg-white flex flex-col gap-8 justify-center items-center p-20">
       <Image
@@ -31,7 +14,7 @@ export default function Loading() {
         priority
         className="w-8 aspect-square"
       />
-      <Progress value={progress} className="w-48 h-2 mb-20" />
+      <Progress value={0} className="w-48 h-2 mb-20" />
       <p className="absolute inset-x-0 bottom-10 text-center px-20">
         For a better experience use Google Chrome or Safari
       </p>
--- src/components/UI/progress.jsx
+++ src/components/UI/progress.jsx
@@ -4,22 +4,38 @@ import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/libs/utils";
+import gsap from "gsap";
 
-const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
+const Progress = React.forwardRef(({ className, value, ...props }, ref) => {
+  const indicatorRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (indicatorRef.current) {
+      gsap.to(indicatorRef.current, {
+        x: `${value || 0}%`,
+        duration: 2,
+        ease: "power3.out",
+      });
+    }
+  }, [value]);
+
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+        className
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        ref={indicatorRef}
+        className="h-full w-full flex-1 bg-primary transition-all"
+        style={{ transform: `translateX(-100%)` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+});
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
