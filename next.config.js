6f8f2547c48f98915aad67c35a1b81b381eeafb4
jfraska <davinfraska20@gmail.com>
2024-09-16 15:16:14 +0700

    fix: oauth popup

--- next.config.js
+++ next.config.js
@@ -13,6 +13,17 @@ const nextConfig = {
       { hostname: "flag.vercel.app" },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "unsafe-none" },
+          { key: "Cross-Origin-Embedder-Policy", value: "unsafe-none" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
